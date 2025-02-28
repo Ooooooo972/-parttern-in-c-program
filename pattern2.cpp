7e20b245a5b8f7d392daf8e4d7d8b713ddd0ceb6
Mahadev Mondal <mondalmahadev816@gmail.com>
2025-02-28 15:49:02 +0530

    new file add

---
+++ pattern2.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,j,n;
+	printf("enter the value of n (1 to 100)");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+		{
+			printf("\n");
+		for(j=i;j<=n;j++)
+		{
+			printf("*");
+		}
+				
+    	}
+	
+return (0);
+}
