b63ca42d256bd8699ecd721f566c3c8af2001d5e
Mahadev Mondal <mondalmahadev816@gmail.com>
2025-03-15 08:03:26 +0530

    Add files via upload

---
+++ pattern3.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,j;
+	
+	for(i=1;i<=9;i++)
+		{
+			printf("\n");
+		for(j=0;j<=(9-i);j++)
+		{
+			printf("*");
+		}
+				
+    	}
+	
+return (0);
+}
