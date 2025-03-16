366ddd058ec34b7f02cffd08bd125ea615a860cc
Mahadev Mondal <mondalmahadev816@gmail.com>
2025-03-16 19:55:47 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+	
+	int a,b,sum,sub,mult;
+	
+	printf("enter the two number a:-,b:-");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	sub=a-b;
+	mult=a*b;
+	printf("\nthe sum  is %d",sum);
+	printf("\nthe subtract number  is %d",sum);
+	printf("\nthe multiply number  is %d",mult);
+	return(0);
+	getch();
+}
+
